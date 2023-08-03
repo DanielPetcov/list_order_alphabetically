a3a8168645d9c27838d950bf0fe44dee2321cd28
DanielPetcov <danielpetcov.work@gmail.com>
2023-08-04 01:03:54 +0300

    writeFunction and final changes

--- hard-level-programing.cpp
+++ hard-level-programing.cpp
@@ -12,11 +12,13 @@ const std::string file_name = "list.txt";
 // functions
 int readFile(std::vector<std::string> &array);
 int orderAlph(std::vector<std::string> &array);
+int writeFile(std::vector<std::string> &array);
 
 int main(){
     std::vector<std::string> list;
     readFile(list);
     orderAlph(list);
+    writeFile(list);
 
     std::cout << "*****************" << '\n';
 
@@ -27,6 +29,8 @@ int main(){
     return 0;
 }
 
+
+// reads the file with the list and makes an array with the items
 int readFile(std::vector<std::string> &array){
 
     std::ifstream file(file_name);
@@ -47,8 +51,27 @@ int readFile(std::vector<std::string> &array){
     return 0;
 }
 
+// orders the array alphabetically
 int orderAlph(std::vector<std::string> &array){
     std::sort(array.begin(), array.end());
+
+    return 0;
+}
+
+int writeFile(std::vector<std::string> &array){
+    std::ofstream file(file_name);
+
+    // checks if the file was open correct
+    if (!file.is_open()) {
+        std::cout << "Error opening file: " << "list.txt" << std::endl;
+        return 1; // Return non-zero to indicate an error
+    }
+
+    for(std::string line : array){
+        file << line << std::endl;
+    }
+
+    file.close();
 
     return 0;
 }
